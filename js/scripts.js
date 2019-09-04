ca560ddc08bb44d663d67a01390da2e5392d88d3
Newton Karanu <karanunewton4@gmail.com>
2019-09-04 23:46:11 +0300

    added scrollspy

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,32 @@ $(document).ready(function(){
     $(this).addClass("active");
 
 });
+
+$(document).ready(function () {
+    // navigation click actions
+    $('.nav-link').on('click', function (event) {
+        event.preventDefault();
+        var sectionID = $(this).attr("data-id");
+        scrollToID('#' + sectionID, 750);
+    });
+    // scroll to top action
+    $('.scroll-top').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    });
+});
+// scroll function
+function scrollToID(id, speed) {
+    var offSet = 50;
+    var targetOffset = $(id).offset().top - offSet;
+    var mainNav = $('#main-nav');
+    $('html,body').animate({ scrollTop: targetOffset }, speed);
+    if (mainNav.hasClass("open")) {
+        mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+        mainNav.addClass("open");
+    }
+}
